efeb684a7d80bdbb812b361537732e935eabc6f8
Hilet Aulas <hilet@HILET.LOCAL>
2024-05-02 08:04:47 -0300

    creacion de api

---
+++ Controllers/CarteleraController.cs
@@ -0,0 +1,32 @@
+using System.Runtime.InteropServices;
+using ATDapi.Responses;
+using ATDapi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ATDapi.Controllers;
+
+
+[ApiController]
+
+public class CarteleraController : ControllerBase
+{
+    public static List<CarteleraModel> DataList = new List<CarteleraModel>();
+
+    [HttpGet]
+    [Route("CarteleraController/Get")]
+
+    public BaseResponse Get()
+    {
+        return new DataResponse<List<CarteleraModel>>(true, (int)HttpStatusCode.OK, "Lista de cartelera", DataList);
+    }
+
+    [HttpPost]
+    [Route ("CarteleraController/Create")]
+    public BaseResponse Post([FromBody]CarteleraModel dataInput)
+    {
+        var l = dataInput;
+        DataList.Add(dataInput);
+        return new BaseResponse(true,(int)HttpStatusCode.Created, "hola");
+    }
+}
---
+++ Models/CarteleraModel.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+namespace ATDapi.Models;
+
+
+public class CarteleraModel
+{
+    public int? id { get; set; } // para que este valor pueda ser null ?
+   public string titulo { get; set; }
+    public string descripcion { get; set; }
+    public string genero { get; set; }
+    public string director { get; set; }
+    public string actores { get; set; }
+    public string url { get; set; } // href ??
+}
