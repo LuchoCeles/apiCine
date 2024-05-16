9d7073c811158dd5080f268e1af39d70ccc365db
Hilet Aulas <hilet@HILET.LOCAL>
2024-05-16 09:37:38 -0300

    archivo json con los datos de cartelera

--- obj/Debug/net8.0/ATDapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ATDapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATDapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d505fcf86aaf2ffde26c6268f764caf18c61ee94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38032188821cdae05e74697576442c1de6eadfd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATDapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATDapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
